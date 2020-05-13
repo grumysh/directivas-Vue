ff5ed4c2dd0491df858fabcbf4164ba208c2b3d0
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-13 08:32:36 -0500

    Se hace uso de v-if

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      name: 'Bitcoin',
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 0
+    }
+  }
+})
