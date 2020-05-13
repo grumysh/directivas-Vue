860331caca88a0c4ec8e587749a4570a0c1f933e
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-13 10:18:45 -0500

    Se listan todas las directivas de Vue.js

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const app = new Vue({
   el: '#app',
   data() {
     return {
-      name: 'Bitcoin',
+      name: 'Directivas en Vue',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 0
+      changePercent: 10,
+      prices: [8400, 7900, 8200, 9000],
+      html: '<p>HTML insertado por v-html</p>',
+      msg: 'Texto insertado por v-text',
+      showPrices: true,
+    }
+  },
+  methods: {
+    toggleHidePrices() {
+      this.showPrices = !this.showPrices;
     }
   }
 })
